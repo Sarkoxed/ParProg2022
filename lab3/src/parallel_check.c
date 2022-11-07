edfed02cc93def959a5ef14d9b838deed7055a22
Sarkoxed <ss2003@yandex.ru>
2022-11-07 07:58:44 +0300

    lab3 - parralel algo check

--- lab3/src/parallel_check.c
+++ lab3/src/parallel_check.c
@@ -4,6 +4,7 @@
 
 void new_array(int*, unsigned int*, int);
 void print_array(int*, int, int);
+void shellsort(int*, int, int);
 
 void new_array(int* array, unsigned int* random_seed, int count){
     srand(*random_seed);
@@ -21,11 +22,28 @@ void print_array(int* array, int count, int t){
     }
     printf("%d]\n", array[count-1]);
 }
+
+void shellsort(int* array, int count, int threads){
+    for(int gap = count/2; gap > 0; gap /= 2){
+#pragma omp parallel for shared(gap, count, array) default(none) num_threads(threads)
+        for(int i = 0; i < gap; i++){
+            for(int part = i + gap; part < count; part += gap){
+                for(int j=part; j>i  && array[j-gap] > array[j]; j-=gap){
+                    int tmp = array[j];
+                    array[j] = array[j-gap];
+                    array[j-gap] = tmp;
+                }
+            }
+        }
+    }
+}
+ 
     
 int main(){
     int n;
+    fprintf(stderr, "The number of array to check: ");
     scanf("%d", &n);
-    const int count = 100000;
+    const int count = 10000;
     unsigned int random_seed = 1337;
     const int num_exp = 10;
     const int thread_bound = 10;
@@ -48,34 +66,13 @@ int main(){
 
     for(threads = 1; threads <= thread_bound; threads++){
         fprintf(stderr, "Number of threads: %d/%d\n", threads, thread_bound);
+
         for(int e = 0; e < num_exp; e++){ 
             fprintf(stderr, "Number of experiment: %d/%d\n", e+1, num_exp);
             array = arrays[threads-1][e];
-            for(int gap = count/2; gap > 0; gap /= 2){
-                for(int i = gap; i < count; i+=gap){
-                    int bnd = (i + gap >= count)?count:i+gap;
-
-                    if (gap >= threads && gap != 1){
-#pragma omp parallel for shared(gap, array, i, bnd) num_threads(threads)
-                        for(int k = i; k < bnd; k++){
-                            for(int j = k; j >= gap && array[j-gap] > array[j] ; j -= gap){
-                                int tmp = array[j];
-                                array[j] = array[j-gap];
-                                array[j-gap] = tmp;
-                            }
-                        }
-                    }else{
-                        for(int k = i; k < bnd; k++){
-                            for(int j = k; j >= gap && array[j-gap] > array[j]; j -= gap){
-                                int tmp = array[j];
-                                array[j] = array[j-gap];
-                                array[j-gap] = tmp;
-                            }
-                        }
-                    }
-                }
-            }
+            shellsort(array, count, threads);
         }
+
         print_array(arrays[threads-1][n], count, threads);
     }
     
